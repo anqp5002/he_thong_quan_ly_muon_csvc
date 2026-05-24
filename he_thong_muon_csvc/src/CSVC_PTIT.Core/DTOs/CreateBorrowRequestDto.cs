32e8d24d4d1520cec188be0fbe752a70cc8a9107
NgnPt <nguyendpham101@gmail.com>
2026-05-24 22:41:39 +0700

    feat: tao BorrowService, IBorrowService, CreateBorrowRequestDto (B3)

---
+++ he_thong_muon_csvc/src/CSVC_PTIT.Core/DTOs/CreateBorrowRequestDto.cs
@@ -0,0 +1,27 @@
+﻿namespace CSVC_PTIT.Core.DTOs;
+
+public class CreateBorrowRequestDto
+{
+    // Thông tin người mượn
+    public int RequesterId { get; set; }
+    public string? ContactPhone { get; set; }
+
+    // Thông tin đơn
+    public string? Title { get; set; }
+    public string? Purpose { get; set; }
+    public string? RequestNote { get; set; }
+
+    // Thời gian
+    public DateTime BorrowStartAt { get; set; }
+    public DateTime BorrowEndAt { get; set; }
+
+    // Danh sách CSVC cần mượn
+    public List<BorrowRequestAssetDto> Assets { get; set; } = new();
+}
+
+public class BorrowRequestAssetDto
+{
+    public int AssetId { get; set; }
+    public int QuantityRequested { get; set; }
+    public string? ItemNote { get; set; }
+}
---
+++ he_thong_muon_csvc/src/CSVC_PTIT.Core/Interfaces/IBorrowService.cs
@@ -0,0 +1,10 @@
+﻿using CSVC_PTIT.Core.DTOs;
+using CSVC_PTIT.Data.Entities;
+
+namespace CSVC_PTIT.Core.Interfaces;
+
+public interface IBorrowService
+{
+    Task<BorrowRequest> CreateInClassRequestAsync(CreateBorrowRequestDto dto);
+    Task<List<BorrowRequest>> GetRequestsByUserAsync(int userId);
+}
