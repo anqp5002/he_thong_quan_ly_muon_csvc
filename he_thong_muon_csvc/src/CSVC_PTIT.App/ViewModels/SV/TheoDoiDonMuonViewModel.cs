5e9ca232a075a8184f3902d9394618153eb456f6
NgnPt <nguyendpham101@gmail.com>
2026-05-28 20:14:50 +0700

    feat: tao TheoDoiDonMuonView va TheoDoiDonMuonViewModel (B4)

---
+++ he_thong_muon_csvc/src/CSVC_PTIT.App/ViewModels/SV/TheoDoiDonMuonViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CSVC_PTIT.Core.Interfaces;
+using CSVC_PTIT.Data.Entities;
+
+namespace CSVC_PTIT.App.ViewModels.SV;
+
+public partial class TheoDoiDonMuonViewModel : ObservableObject
+{
+    private readonly IBorrowService _borrowService;
+
+    [ObservableProperty]
+    private ObservableCollection<BorrowRequest> _danhSachDon = new();
+
+    [ObservableProperty]
+    private BorrowRequest? _donDuocChon;
+
+    [ObservableProperty]
+    private bool _dangTai;
+
+    // Tạm thời hardcode userId = 1 để test, sau này sẽ lấy từ session đăng nhập
+    private readonly int _currentUserId = 1;
+
+    public TheoDoiDonMuonViewModel(IBorrowService borrowService)
+    {
+        _borrowService = borrowService;
+    }
+
+    [RelayCommand]
+    private async Task TaiDanhSachAsync()
+    {
+        DangTai = true;
+        var list = await _borrowService.GetRequestsByUserAsync(_currentUserId);
+        DanhSachDon = new ObservableCollection<BorrowRequest>(list);
+        DangTai = false;
+    }
+}
