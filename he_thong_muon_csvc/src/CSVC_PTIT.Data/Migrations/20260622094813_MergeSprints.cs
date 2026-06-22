b938d566db5edd26f5554b71356654168dd10a49
Pham An <anpham5002@gmail.com>
2026-06-22 16:48:23 +0700

    chore: add EF Core migration after merging all sprints

---
+++ he_thong_muon_csvc/src/CSVC_PTIT.Data/Migrations/20260622094813_MergeSprints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSVC_PTIT.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MergeSprints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
